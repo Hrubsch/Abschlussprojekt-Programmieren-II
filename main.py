Fixed calculation

main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-#from Akkumodell2.battery_simulator_start import BatterySimulator
-#from Akkumodell2.battery_pack_start import BatteryPack
-#from Akkumodell2.Akku import lifepo
-#from Akkumodell2.Akku import nmc
+from Akkumodell2.battery_simulator_start import BatterySimulator
+from Akkumodell2.battery_pack_start import BatteryPack
+from Akkumodell2.Akku import lifepo
+from Akkumodell2.Akku import nmc
 
 
 import matplotlib.pyplot as plt
@@ -142,43 +142,50 @@ if __name__ == "__main__":
     df["dt"] = df["time_s"].diff() # Zeitdifferenz
     df = df[df["dt"] >= 1].copy()
     df["s"] = df["ds"].cumsum()
-    df["v"] = np.gradient(df["s"], df["time_s"])
+
+    #df["v"] = np.gradient(df["s"], df["time_s"])
     #df["v"] = df["v"].rolling(window=6, center=True, min_periods=1).mean() # Glättung der Geschwindigkeit
+
     df["v"] = df["ds"] / df["dt"] # Geschwindigkeit
-    df.loc[0, "v"] = 0 # erste Zeile korrigieren
     df.loc[df["v"] > 30, "v"] = np.nan # Geschwindigkeit > 30 m/s löschen
     df["v"] = df["v"].interpolate() # fehlende Werte interpolieren
-    df["v"] = (df["v"].rolling(window=6, center=True, min_periods=1).mean()) # Glättung der Geschwindigkeit
+    df["v"] = (df["v"].rolling(window=25, center=True, min_periods=1).mean()) # Glättung der Geschwindigkeit
 
     df["a"] = np.gradient(df["v"], df["time_s"])
-    #df["a"] = df["a"].rolling(window=6, center=True, min_periods=1).mean() # Glättung der Beschleunigung
-    df.loc[0, "a"] = 0 # erste Zeile korrigieren
-    df.loc[df["a"] > 2, "a"] = np.nan # Beschleunigung > 2 m/s² löschen
-    df.loc[df["a"] < -2, "a"] = np.nan # Beschleunigung < -2 m/s² löschen
+    df.loc[df["a"] > 1, "a"] = np.nan # Beschleunigung > 2 m/s² löschen
+    df.loc[df["a"] < -1, "a"] = np.nan # Beschleunigung < -2 m/s² löschen
     df["a"] = df["a"].interpolate()
+    df["a"] = df["a"].rolling(window=25, center=True, min_periods=1).mean() # Glättung der Beschleunigung
 
 
 
     #df["a"] = df["v"].diff() / df["dt"] # Beschleunigung
     df["dh"] = df["ele_glatt"].diff()                   # Höhenänderung
     df["phi_rad"] = np.arctan2(df["dh"], df["ds"])      # Steigungswinkel
+    df.loc[df["phi_rad"] < -0.174533, "phi_rad"] = np.nan # Beschleunigung > 2 m/s² löschen
+    df["phi_rad"] = df["phi_rad"].interpolate() # fehlende Werte interpolieren
+    df["phi_rad"] = df["phi_rad"].rolling(window=25, center=True, min_periods=1).mean() # Glättung des Steigungswinkels
     df["phi_grad"] = np.degrees(df["phi_rad"])          # Steigungswinkel in Grad
     df["F_D"] = 0.5 * rho * A * df["v"]**2              # Luftwiderstand
     df["F_H"] = (m * g) * np.sin(df["phi_rad"])         # Hangkraft
     df["F_A"] = m * df["a"]                             # Beschleunigungskraft
     df["F_Antrieb"] = df["F_D"] + df["F_H"] + df["F_A"] # Gesamte Antriebskraft
+    df["F_Antrieb"] = df["F_Antrieb"].rolling(window=25, center=True, min_periods=1).mean() # Glättung der Antriebskraft
     df["P"] = df["F_Antrieb"] * df ["v"]                # Berechnung der Leistung
+    df["P"] = df["P"].rolling(window=25, center=True, min_periods=1).mean() # Glättung der Leistung
     df["T_drehmoment"] = df["F_Antrieb"] * (r_m/2)      # Berechnung Drehmoment am Motor in Nm
+    df["T_drehmoment"] = df["T_drehmoment"].rolling(window=25, center=True, min_periods=1).mean() # Glättung des Drehmoments
     df["I_motor"] = df["T_drehmoment"] / m_konst        # Berechnung Motorstrom bei bekannter Motorkonstante
+    df["I_motor"] = df["I_motor"].rolling(window=25, center=True, min_periods=1).mean() # Glättung des Motorstroms   
 
 
-    #b1 = lifepo(capacity_nom_cell_Ah=10.0, initial_soc=1.0)
-    #b2 = nmc(capacity_nom_cell_Ah=10.0, initial_soc=1.0)
-    #simulatorb1 = BatterySimulator(b1)
-    #simulatorb2 = BatterySimulator(b2)
+    b1 = lifepo(capacity_nom_cell_Ah=10.0, initial_soc=1.0)
+    b2 = nmc(capacity_nom_cell_Ah=10.0, initial_soc=1.0)
+    simulatorb1 = BatterySimulator(b1)
+    simulatorb2 = BatterySimulator(b2)
 
-    #simulatorb1.simulation_ladezustand(df)
-    #simulatorb1.plot_ladezustand(df)
+    simulatorb1.simulation_ladezustand(df)
+    simulatorb1.plot_ladezustand(df)
 
 
 # Ergebnisse speichern
@@ -201,7 +208,7 @@ if __name__ == "__main__":
 
 
     for spalte in df.columns:
-        if spalte in ["lat", "lon", "time", "ds", "dt", "F_D", "F_H", "F_A", "dh", "temp", "phi_rad", "lat_glatt", "lon_glatt", "time_s"]:
+        if spalte in ["lat", "lon", "time", "ele", "ele_glatt", "ds", "dt", "dh","F_D", "F_H", "F_A", "F_Antrieb", "temp", "phi_rad", "lat_glatt", "lon_glatt", "time_s"]:
             continue
 
         plt.figure(figsize=(10, 5))